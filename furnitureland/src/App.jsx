ff359edb7a78f58a9375bf557263f304956a5bc9
iamsahilr <srajbhandari63@gmail.com>
2024-05-28 11:14:40 -0500

    footer completed

--- furnitureland/src/App.jsx
+++ furnitureland/src/App.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
 import Carousel from './components/Carousel/Carousel';
 import MainContent from './components/MainContent/MainContent';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
@@ -12,6 +13,8 @@ const App = () => {
       <Carousel />
 
       <MainContent />
+
+      <Footer />
       
     </div>
   )
---
+++ furnitureland/src/components/Footer/Footer.jsx
@@ -0,0 +1,64 @@
+import './Footer.css'
+import logo from '../../assets/furniturelandlogo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import {faPhone} from '@fortawesome/free-solid-svg-icons'
+import {faLocationDot} from '@fortawesome/free-solid-svg-icons'
+
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className="innerfooter">
+            <div className="company-links">
+                <p>
+                    <img src={logo} alt="company logo" style={{width:'75px', height:'75px'}}/>
+                </p>
+                <p className='footer-company-links'>
+                    <FontAwesomeIcon icon={faEnvelope}/>&nbsp; &nbsp;furnitureland@gmail.com</p>
+                <p className='footer-company-links'>
+                    <FontAwesomeIcon icon={faPhone}/>&nbsp; &nbsp;01-4224797</p>
+                <p className='footer-company-links'>
+                    <FontAwesomeIcon icon={faLocationDot}/>&nbsp; &nbsp;Bluestar Complex, Tripureshwor, Kathmandu 44600, Nepal</p>
+            </div>
+            <div className="quicklinks">
+                <h5>CONTACT</h5>
+                <ul>
+                    <li>Contact us</li>
+                    <li>Book an appointment</li>
+                    <li>Showrooms</li>
+                </ul>
+            </div>
+
+            <div className="quicklinks">
+                <h5>Shop by category</h5>
+                <ul>
+                    <li>Living</li>
+                    <li>Kithcen & Dining</li>
+                    <li>Bedroom</li>
+                    <li>Office</li>
+                    <li>Garden</li>
+                    <li>Decorations</li>
+                </ul>
+            </div>
+
+            <div className="quicklinks">
+                <h5>Terms & Conditions</h5>
+                <ul>
+                    <li>Privacy Policy</li>
+                    <li>Return Policy</li>
+                    <li>Terms & Conditions</li>
+                    <li>FAQs</li>
+                </ul>
+            </div>
+           
+        </div>
+
+        <div className="copyright">
+            <sup>&copy;</sup>Copyright 2024, &nbsp; Furniture Land Store, &nbsp; Nepal
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
