abc2cbe924ba3dd60403d3735bf5f8462b168868
iamsahilr <srajbhandari63@gmail.com>
2024-05-30 13:42:47 -0500

    products filter by category

--- furnitureland/src/pages/ProductPage.jsx
+++ furnitureland/src/pages/ProductPage.jsx
@@ -6,61 +6,73 @@ import axios from 'axios'
 import Card from 'react-bootstrap/Card';
 import prod1 from "../assets/prod1.png"
 
-
 const Product = () => {
-
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                const res = await axios.get("http://localhost:8080/products");
-                setProducts(res.data);
-            } catch (err) {
-                console.log(err);
-            }
-        };
-        fetchProducts();
-    }, []);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await axios.get("http://localhost:8080/products");
+        setProducts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchProducts();
+  }, []);
 
-  const [categories, setCategories] = useState([])
-
-  useEffect(()=>{
-    const fetchAllCategories = async ()=>{
-      try{
+  useEffect(() => {
+    const fetchAllCategories = async () => {
+      try {
         const res = await axios.get("http://localhost:8080/categories")
         setCategories(res.data)
-      }catch(err){
+      } catch (err) {
         console.log(err)
       }
     }
     fetchAllCategories()
-  },[])
+  }, []);
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
 
   return (
     <>
       <Header />
       <section className='section-product'>
         <div className="category-list">
-          
           <ul>
-            {categories.map(category=>(
-              <li>{category.name}</li>
+            <li>All</li>
+            {categories.map(category => (
+              <li 
+                key={category.id} 
+                onClick={() => handleCategoryClick(category)}
+                className={selectedCategory && selectedCategory.id === category.id ? 'selected-category' : ''}
+              >
+                {category.name}
+              </li>
             ))}
-            
           </ul>
         </div>
 
         <div className="product-cards-container">
-        {products.map(product => (
-            <Card key={product.id} style={{ margin:'20px 10px', width: '25%', borderRadius: '0px', border: '1px solid #d1e2e3' }}>
-                <Card.Img style={{ borderRadius: '0px', height: '280px' }} variant="top" src={prod1} />
+          {products
+            .filter(product => !selectedCategory || product.category_id === selectedCategory.id)
+            .map(product => (
+              <Card key={product.id} style={{ margin: '20px 10px', width: '20%', borderRadius: '0px', border: '1px solid #d1e2e3' }}>
+                <Card.Img style={{ borderRadius: '0px', height: '200px' }} variant="top" src={prod1} />
                 <Card.Body>
-                    <Card.Title className='prod-title'>{product.name}</Card.Title>
-                    <Card.Text className='prod-price'>${product.price}</Card.Text>
+                  <Card.Title className='prod-title'>{product.name}</Card.Title>
+                  <Card.Text className='prod-desc'>Brand: {product.brand_id}</Card.Text>
+                  <Card.Text className='prod-desc'>Dimension: {product.dimension}</Card.Text>
+                  <Card.Text className='prod-desc'>Material: {product.materials}</Card.Text>
+                  <Card.Text className='prod-price'>${product.price}</Card.Text>
                 </Card.Body>
-            </Card>
-        ))}
+              </Card>
+            ))}
         </div>
       </section>
       <Footer />
