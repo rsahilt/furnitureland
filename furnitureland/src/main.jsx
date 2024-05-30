cb4c60a3c56f1fa5b1daf2f8cbdc4dd41c4fcb43
iamsahilr <srajbhandari63@gmail.com>
2024-05-29 22:30:24 -0500

    static category

--- furnitureland/src/main.jsx
+++ furnitureland/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import App from './App.jsx'
 import About from './pages/AboutPage.jsx'
 import Showroom from './pages/Showroom.jsx'
 import Contact from './pages/ContactPage.jsx'
--- furnitureland/src/pages/ProductPage.jsx
+++ furnitureland/src/pages/ProductPage.jsx
@@ -2,45 +2,22 @@ import './ProductPage.css'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 import { useEffect, useState } from 'react'
-import axios from 'axios';
 
 
 const Product = () => {
-
-  const[category, setCategory] = useState([])
-
-  useEffect(() => {
-    axios.get('http://localhost:8081')
-    .then(res =>setCategory(res.data))
-    .catch(err => console.log(err));
-  },[])
-
   return (
     <>
       <Header />
       <section className='section-product'>
-        
-        <table>
-
-          <thead>
-            <tr>
-              <th>Name</th>
-            </tr>
-          </thead>
-
-          <tbody>
-          {
-              category.map((data, i) => (
-                <tr key={i}>
-                  <td>{data.Name}</td>
-                </tr>
-              ))
-            }
-            
-          </tbody>
-
-        </table>
-
+        <div className="category-list">
+          <ul>
+            <li>Living</li>
+            <li>Dining</li>
+            <li>Bedroom</li>
+            <li>Kitchen</li>
+            <li>Garden</li>
+          </ul>
+        </div>
       </section>
       <Footer />
     </>
