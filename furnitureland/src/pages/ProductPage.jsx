f6060fc3f247b4b07f91056cb75862679c09f998
iamsahilr <srajbhandari63@gmail.com>
2024-05-31 11:45:32 -0500

    products page sidebar

--- furnitureland/src/pages/ProductPage.jsx
+++ furnitureland/src/pages/ProductPage.jsx
@@ -41,31 +41,57 @@ const Product = () => {
     fetchAllCategories()
   }, []);
 
-  const handleCategoryChange = (event) => {
-    const categoryId = event.target.value;
-    setSelectedCategory(categoryId === 'All' ? 'All' : categories.find(category => category.id === parseInt(categoryId)));
+  const handleCategoryClick = (categoryId) => {
+    setSelectedCategory(categoryId === 'All' ? 'All' : categories.find(category => category.id === categoryId));
   };
+
+  const filteredProducts = products.filter(product => selectedCategory === 'All' || product.category_id === selectedCategory.id);
 
   return (
     <>
       <Header />
-      <section className='section-product'>
-        <div className="category-dropdown">
-          <select onChange={handleCategoryChange} value={selectedCategory.id || 'All'}>
-            <option value="All">All Furnitures</option>
-            {categories.map(category => (
-              <option key={category.id} value={category.id}>
-                {category.name}
-              </option>
-            ))}
-          </select>
+      <section className='section-product' >
+        <div className="product-sidebar">
+          <div className="category-list">
+            <ul>
+              <li>Furnitures</li>
+              <li 
+                onClick={() => handleCategoryClick('All')}
+                className={selectedCategory === 'All' ? 'selected-category' : ''}
+              >
+                All Furnitures
+              </li>
+              {categories.map(category => (
+                <li 
+                  key={category.id} 
+                  onClick={() => handleCategoryClick(category.id)}
+                  className={selectedCategory.id === category.id ? 'selected-category' : ''}
+                >
+                  {category.name}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
 
-        <div className="product-cards-container">
-          {products
-            .filter(product => selectedCategory === 'All' || product.category_id === selectedCategory.id)
-            .map(product => (
-              <Card key={product.id} data-aos="fade-in" style={{ margin: '20px 10px', width: '300px', borderRadius: '0px', border: '1px solid #d1e2e3' }}>
+        <div className="prod-main-content">
+
+        <div className="category-menu " style={{ margin: '0 auto', width: '95%', fontSize: '1.3em', padding: '10px' }}>
+          <div className='rectangle-triangle'>
+            Furnitures 
+          </div>
+          &nbsp; &nbsp;
+          <div className='triangle-rectangle'>
+            {selectedCategory === 'All' ? 'All Furnitures' : selectedCategory.name}
+          </div>
+        </div>
+          <div style={{margin:'0 auto', width:'95%', fontSize:'1.3em', padding:'10px'}}>
+            <h6>Showing {filteredProducts.length} item(s) in <strong>{selectedCategory === 'All' ? 'All Furnitures' : selectedCategory.name}</strong></h6>
+          </div>
+
+          <div className="product-cards-container">
+            {filteredProducts.map(product => (
+              <Card key={product.id} data-aos="fade-in" style={{ margin: '10px 10px', width: '300px', borderRadius: '0px', border: '1px solid #d1e2e3' }}>
                 <Card.Img style={{ borderRadius: '0px', height: '300px' }} variant="top" src={prod1} />
                 <Card.Body>
                   <Card.Title className='prod-title'>{product.name}</Card.Title>
@@ -74,6 +100,7 @@ const Product = () => {
                 </Card.Body>
               </Card>
             ))}
+          </div>
         </div>
       </section>
       <Footer />
